e0a28fc4ebf76b11c02a951ca0a3b31c9aaa555d
wangq <525043809@qq.com>
2019-01-11 17:11:40 +0800

    Update brahma config.

--- app/src/main/java/io/brahmaos/wallet/brahmawallet/common/BrahmaConfig.java
+++ app/src/main/java/io/brahmaos/wallet/brahmawallet/common/BrahmaConfig.java
@@ -32,7 +32,7 @@ public class BrahmaConfig {
     private static final String KEY_TOKEN_LIST_VERSION = "token.list.version";
 
     // false: main net; true: ropsten testnet;
-    public static boolean debugFlag = true;
+    public static boolean debugFlag = false;
     private String networkUrl;
     private String languageLocale;
     private String currencyUnit;
