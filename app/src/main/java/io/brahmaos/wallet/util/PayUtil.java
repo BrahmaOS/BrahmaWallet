8ffa72cf12f0db8671e8824f4de00674717944ce
Azalea <azalea.pan@brahmaos.io>
2019-03-22 18:02:51 +0800

    Change payment status's view color.
    Signed-off-by: Azalea <azalea.pan@brahmaos.io>

--- app/src/main/java/io/brahmaos/wallet/util/PayUtil.java
+++ app/src/main/java/io/brahmaos/wallet/util/PayUtil.java
@@ -13,19 +13,19 @@ public class PayUtil {
         switch (status) {
             case 1:
                 tvPayStatus.setText(context.getString(R.string.pay_trans_status_unpaid));
-                tvPayStatus.setTextColor(context.getColor(R.color.tx_send));
+                tvPayStatus.setTextColor(context.getColor(R.color.payment_to_be_paid));
                 return;
             case 2:
                 tvPayStatus.setText(context.getString(R.string.pay_trans_status_confirm));
-                tvPayStatus.setTextColor(context.getColor(R.color.tx_send));
+                tvPayStatus.setTextColor(context.getColor(R.color.payment_confirmation));
                 return;
             case 3:
                 tvPayStatus.setText(context.getString(R.string.pay_trans_status_succ));
-                tvPayStatus.setTextColor(context.getColor(R.color.color_hint_text));
+                tvPayStatus.setTextColor(context.getColor(R.color.payment_successful));
                 return;
             case 4:
                 tvPayStatus.setText(context.getString(R.string.pay_trans_status_fail));
-                tvPayStatus.setTextColor(context.getColor(R.color.tx_error));
+                tvPayStatus.setTextColor(context.getColor(R.color.payment_failure));
                 return;
             default:
                 return;
